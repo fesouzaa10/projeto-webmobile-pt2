29bbcdbb260436a34fd5565b51ea3bf3de3394d9
fesouzaa10 <felipecassemiro15@gmail.com>
2026-05-07 10:43:34 +0000

    nova página

---
+++ projeto_pt2/src/app/pesquisar/page.js
@@ -0,0 +1,38 @@
+<html lang="pt-br">
+<head>
+<title>Pesquisar Material</title>
+</head>
+
+<body>
+
+<header>
+  <h1>ReciclaFácil ♻</h1>
+  <p>Aprenda a reciclar corretamente</p>
+
+  <nav>
+    <a href="index.html">Início</a>
+    <a href="pesquisar.html">Pesquisar</a>
+  </nav>
+</header>
+
+<main>
+
+<section className="pesquisa">
+  <h2>Pesquisar Material</h2>
+
+  <form className="search-box" onsubmit="return false;">
+    <input type="text" placeholder="Digite um material..."/>
+    <button type="submit">Pesquisar</button>
+  </form>
+
+  <p id="resultado"></p>
+</section>
+
+</main>
+
+<footer>
+  <p>Felipe de Souza Cassemiro</p>
+  <p>RA: 10735839</p>
+</footer>
+</body>
+</html>
